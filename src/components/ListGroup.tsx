453df448a41165ac3f233c3db84bc8ebbf0019b7
Randika17877 <randikakavindu35@gmail.com>
2026-09-12 15:22:39 +0530

    feat: update ListGroup component to display dynamic items and handle clicks

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,34 @@
-
+import { MouseEvent } from "react";
 
 function ListGroup() {
+
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    const handleClick = (event: MouseEvent) => console.log(event)
+
     return (
         <>
-            <h1>List Group</h1>
+            <h1>List</h1>
+
+            
 
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                { items.length === 0 ? <p>No item found</p> : null }
+                {items.map((item, index) => (
+                    <li 
+                    className="list-group-item" 
+                    key={item} 
+                    onClick={handleClick}>
+                        {item}
+                    </li>
+                ))}
+
             </ul>
         </>
     );
