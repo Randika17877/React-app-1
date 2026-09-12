d1790a2d36f9c51f28ab53af9eece3c098ad23d4
Randika17877 <randikakavindu35@gmail.com>
2026-09-12 16:13:30 +0530

    feat: update ListGroup component to accept dynamic items and heading

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,27 +1,20 @@
 import { useState } from "react";
 
+interface Props {
+    items: string[];
+    heading: string;
+}
 
-function ListGroup() {
-
-    let items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
+function ListGroup({items, heading}: Props) {
 
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     
     return (
         <>
-            <h1>List</h1>
-
-            
-
-            <ul className="list-group">
-                { items.length === 0 ? <p>No item found</p> : null }
+            <h1>{heading}</h1>
+            { items.length === 0 ? <p>No item found</p> : null}
+            <ul className="list-group">        
                 {items.map((item, index) => (
                     <li 
                     className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' } 
