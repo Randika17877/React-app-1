3e47310467117eb00ec8c7d0214b0087f3aec67a
Randika17877 <randikakavindu35@gmail.com>
2026-09-12 23:18:56 +0530

    feat: replace Alert component with Button component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 function App() {
 
     return (
         <div>
-            <Alert>
-                <span>Hello World</span>
-            </Alert>
+            <Button  onClick={() => console.log('clicked')}>
+                My Button
+            </Button>
         </div>
     );
 
