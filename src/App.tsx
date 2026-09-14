23c0b56b76fb471a91edbaad42290903584d91c7
Randika17877 <randikakavindu35@gmail.com>
2026-09-14 15:17:37 +0530

    feat: refactor ListGroup component and update App to use new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGroup/ListGroup";
+import './App.css';
 function App() {
 
-    const [alertVisible, setAlertVisibility] = useState(false);
+    const items=['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
 
     return (
         <div>
-            { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>
-                My Alert
-            </Alert>}
-            <Button  onClick={() => setAlertVisibility(true)}>
-                My Button
-            </Button>
+            
+            <ListGroup 
+            items={items}
+            heading="Cities"
+            onSelectItem={(item) => console.log(item)}
+            />
         </div>
     );
 
